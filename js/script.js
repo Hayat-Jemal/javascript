df51627659cc64ab01f4ae2f245b3d4c7b1ab6b5
Hayat Jemal <htjemal2000@gmail.com>
2021-11-08 23:04:07 +0300

    add something

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ el.addEventListener("click", function () {
     el.innerHTML = "changed";
 })
 
-console.log("hello hello")
+console.log("hello")
 
 document.write("I have comments in my JavaScript code!") 
 // document.write("You can't see this!");
